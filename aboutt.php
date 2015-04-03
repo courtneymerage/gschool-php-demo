ad61e84005b866e378bd08cedb96895adfe71cb8
courtneymerage <courtneymerage@gmail.com>
2015-04-02 20:08:50 -0600

    moving index.html to php

---
+++ aboutt.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Adding the Page Title from the index, about, or contact pages -->
+    <title>About | Your Name Here</title>
+
+    <!-- Tell the browser to render the view width correctly -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Link our stylesheet -->
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body id="about">
+
+    <header>
+        <div class="row clearfix">
+
+            <!-- Our Logo -->
+            <a href="index.html">
+                <img class="logo" src="images/logo.png" />
+            </a>
+
+            <!-- Our Site Navigation -->
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="aboutt.php">About</a>
+                <a href="contactt.php">Contact</a>
+            </nav>
+
+        </div>
+    </header>
+    
+    <div class="row clearfix">
+    
+        <!-- Content Area that is a two third column -->
+<div class="column two-third">
+    <h3>Column Heading</h3>
+
+    <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+    <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+    </div>
+    
+    
+    <!-- sidebar area & widgets -->
+    <div class="column one-third sidebar">
+    <!--begin widget -->
+        <div class="widget">
+        <h3>Widget Title</h3>
+            <p>Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle-ly playthings Wickersham Brothers guaranteed murmur.</p>
+            </div>
+        <!--end widget-->
+        
+        <!--begin widget-->
+        <div class="widget">
+                <h3>Widget Title</h3>
+            <p>Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle-ly playthings Wickersham Brothers guaranteed murmur.</p>    
+        </div>
+        <!-- end widget-->
+        
+         <!--begin widget-->
+        <div class="widget">
+                <h3>Widget Title</h3>
+            <p>Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle-ly playthings Wickersham Brothers guaranteed murmur.</p>    
+        </div>
+        <!-- end widget-->
+        
+    </div>
+        </div>
+    
+    <footer>
+        <!-- Out Site Navigation -->
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="aboutt.php">About</a>
+            <a href="contactt.php">Contact</a>
+        </nav>
+
+        <!-- Site Copyright -->
+        <p>Copyright 2015 - Your Name Here</p>
+    </footer>
+
+</body>
+
+</html>
